cb1d471c01d7066287c5a4b4349f87f61101590b
Sam <mmrmas@hotmail.com>
2024-04-17 19:29:03 +0200

    Delete _archive directory

--- _archive/ipt/src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-import IPToken from './IPToken.json';
-import IPtrade from './IPtrade.json'
-
-export const contract_t_Abi = IPToken.abi;
-export const contract_t_Address = '0xF2232724971Dc4f1a90E9786445dc6945932E44e';
-
-
-
